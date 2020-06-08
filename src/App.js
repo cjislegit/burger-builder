e4b785a40bfe16c245fad3dbbbf17a40464a545f
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-06-07 21:36:57 -0700

    Created BurgerBuilder component and adding some styling to Layout component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import Layout from './components/Layout/Layout';
 import './App.css';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div className='App'>
-      <Layout />
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     </div>
   );
 }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import classes from './Layout.module.css';
+
 const layout = (props) => {
   return (
     <React.Fragment>
       <div>Toolbar, SideDrwawer, Dackdrop</div>
-      <main>{props.children}</main>
+      <main className={classes.content}>{props.children}</main>
     </React.Fragment>
   );
 };
