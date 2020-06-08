b7b24bb03e4aadc081f7bb29b28930f7a4de1a04
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-06-07 21:14:05 -0700

    Create a Layout component and added it to the the App file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Layout from './components/Layout/Layout';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Layout />
+    </div>
+  );
 }
 
 export default App;
