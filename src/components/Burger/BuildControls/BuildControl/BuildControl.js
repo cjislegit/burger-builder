f3375fe154e987ed2c23d1f0f2b9ea94aa114360
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-06-09 19:45:32 -0700

    Started building build controls

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const buildControl = (props) => {
+    return (  );
+}
+ 
+export default buildControl;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,8 +4,8 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 0,
-      bacon: 0,
+      salad: 1,
+      bacon: 1,
       cheese: 0,
       meat: 0,
     },
