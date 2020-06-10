4b401fe6a181cfe8061c1437935928e983fdba5b
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-06-09 18:58:55 -0700

    If there are no ingredients div will be returned

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,20 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './Burgeringrients/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  // Turns object into array
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    // Flattens the array so I can check if its empty
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please Add Ingredients</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='breadTop' />
