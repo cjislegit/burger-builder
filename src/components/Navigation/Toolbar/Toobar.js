09cfc453a726058d66e1d25feb6f7339a59f6240
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-06-15 20:09:24 -0700

    Added nav buttons to the toolbar

--- src/components/Navigation/Toolbar/Toobar.js
+++ src/components/Navigation/Toolbar/Toobar.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import Navigation from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
       <div>MENU</div>
       <Logo />
-      <nav>...</nav>
+      <Navigation />
     </header>
   );
 };
