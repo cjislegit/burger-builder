a7303339969095958baf60e006a5388723b096ef
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-06-16 20:22:57 -0700

    Created side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toobar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => {
   return (
     <React.Fragment>
       <Toolbar />
+      <SideDrawer />
       <main className={classes.content}>{props.children}</main>
     </React.Fragment>
   );
