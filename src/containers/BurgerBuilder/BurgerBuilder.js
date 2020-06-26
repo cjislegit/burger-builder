8695ef59c870f366195e1ba27e7d94a49aef4cf5
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-06-25 20:23:18 -0700

    Added module for error handling and applied it to burgerbuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/spinner';
+import witherrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -143,4 +144,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default witherrorHandler(BurgerBuilder, axios);
